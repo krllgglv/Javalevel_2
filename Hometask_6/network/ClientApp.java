b6d4adf956e7c64d5129d6ee742eafe016e60a24
krllgglv <kirillgogolev@yandex.ru>
2020-12-06 00:47:00 +0300

    l2_ht6

---
+++ Hometask_6/network/ClientApp.java
@@ -0,0 +1,7 @@
+package network;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        new Client();
+    }
+}
---
+++ Hometask_6/network/ServerApp.java
@@ -0,0 +1,7 @@
+package network;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
